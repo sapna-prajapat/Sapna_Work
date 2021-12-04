16df3d3ef22af3d3d3be06e6a62d8c866ebf7c55
sapna <sapnaprajapat079@gmail.com>
2021-12-04 17:20:16 +0530

    token commit

---
+++ Addition_Matrix.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+  int r,c;
+  int a[2][2]={{10,20},{30,40}};
+  int b[2][2]={{50,20},{30,40}};
+  int d[2][2];
+      printf("Given Matrix 1---------\n");
+       for(r=0;r<2;r++)
+        {
+          for(c=0;c<2;c++)
+           printf("%4d \n",a[r][c]);
+        }
+      printf("\n");
+  
+      printf("Given Matrix 2---------\n");
+        for(r=0;r<2;r++)
+         {
+           for(c=0;c<2;c++)
+             printf("%4d \n",b[r][c]);
+         }
+      printf("\n");
+  
+      printf("After Addition---------\n");
+        for(r=0;r<2;r++)
+         {
+            for(c=0;c<2;c++){
+              d[r][c]=a[r][c]+b[r][c];
+              printf("%4d \n",d[r][c]);
+         }
+     }
+       printf("\n");
+  return 0;
+} 
